22bf3342a2a70f97b5e363b1b2df5366e0ebb36c
ping <513011631@qq.com>
2018-11-24 17:57:09 +0800

    调整css样式和js逻辑

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var hashA = init()
 var key = hashA["key"]
 var hash = hashA["hash"]
-
+var lock = false
 
 
 //2.遍历key 生成kbd标签及内部结构
@@ -19,16 +19,19 @@ function generateKeyboard(key, hash) {
             var button1 = tag('img', { id: row[index2],className: 'button' })
             button1.src='./edit.png'
             button1.onclick = function (xzkjcnxlkcjlk) {
+                lock =true
                 var button2 = xzkjcnxlkcjlk.target
                 var img2 = button2.previousSibling
                 var key2 = button2.id
                 var x = prompt('给我个网址')
+                if(x === null){return}
                 hash[key2] = x
                 img2.src = 'http://' + x + '/favicon.ico'
                 img2.onerror = function (error) {
                     error.target.src = ''
                 }
                 localStorage.setItem('aa', JSON.stringify(hash))
+                setTimeout(()=>lock=false,4)
             }
 
             var image = tag('img', { className: 'img' })
@@ -60,9 +63,17 @@ function listenToUser(hash) {
     }
     document.addEventListener('click',function(e){
         if(e.target.id){
+            
             let key = e.target.id
             web = hash[key]
-            window.open('http://' + web, '_blank')
+            
+            if(web){
+                web = web.trim()
+                console.log(web)
+                if(!lock){
+                    window.open('http://' + web, '_blank')
+                }
+            }
         }
     })
 }
@@ -78,39 +89,39 @@ function getFromLocalStorage(name) {
 function init() {
     var key = {
         //添加上数字键和源代码键
-        '0': { 0: '1', 1: '2', 2: '3', 3: '4', 4: '5', 5: '6', 6: '7', 7: '8', 8: '9', 9: '0', length: 10 },
+        '0': { 0: '1', 1: '2', 2: '3', 3: '4', 4: '5', 5: '6', 6: '7', 7: '8', 8: '9', 9: '0',10:'$' ,length: 11 },
         '1': { 0: 'q', 1: 'w', 2: 'e', 3: 'r', 4: 't', 5: 'y', 6: 'u', 7: 'i', 8: 'o', 9: 'p', length: 10 },
         '2': { 0: 'a', 1: 's', 2: 'd', 3: 'f', 4: 'g', 5: 'h', 6: 'j', 7: 'k', 8: 'l', length: 9 },
         '3': { 0: 'z', 1: 'x', 2: 'c', 3: 'v', 4: 'b', 5: 'n', 6: 'm', length: 7 },
         length: 4
     }
     var hash = {
-        'q': 'qq.com',
+        'q': 'www.qq.com',
         'w': 'weibo.com',
-        'e': '',
-        'r': 'renren.com',
+        'e': 'ele.me',
+        'r': 'sns.renren.com',
         't': 'taobao.com',
-        'y': 'youku.com',
-        'u': '',
+        'y': 'www.youku.com',
+        'u': 'www.uc.cn',
         'i': 'www.iqiyi.com',
-        'o': '',
-        'p': '',
+        'o': 'www.360.cn',
+        'p': 'www.apple.com/cn',
         'a': 'amazon.com',
         's': 'souhu.com',
         'd': 'dangdang.com',
         'f': 'facebook.com',
         'g': 'google.com',
-        'h': 'jd.com',
+        'h': 'www.jd.com',
         'j': 'juejin.im',
-        "k": 'tianmao.com',
+        "k": 'www.tmall.com',
         'l': 'douban.com',
         'z': 'zhihu.com',
-        'x': 'xinlang.com',
+        'x': 'www.mi.com',
         'c': 'baidu.com',
-        'v': 'meituan.com',
-        'b': '360.com',
+        'v': 'cn.vuejs.org',
+        'b': 'baidu.com',
         'n': 'souhu.com',
-        'm': 'sogou.com'
+        'm': 'meituan.com'
     }
     var hashNew = getFromLocalStorage('aa')
     if (hashNew) {
